a266813e900aa1613411e003df4ca3991fbf073e
Sateesh Chowdari <spchowdari@gmail.com>
2025-05-13 13:54:06 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <HomePage>
    
    with the text content:
    
        Generate Your JEE Mock PaperTailor questions to your study needs. Select subject, topics, difficulty, and number of questions.SubjectSelect a subjectDifficultySelect difficultyTopicsSelect a subject firstSelect relevant topics for the paper.Please select at least one topic.Number of QuestionsGenerate PaperNo paper generated yetFill out the form above to create your personalized JEE mock paper.
    
    Relevant files:
    
        - https://6000-firebase-studio-1747035172961.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-firebase-studio-1747035172961.cluster-ejd22kqny5htuv5dfowoyipt52.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    decrease the width so that there is space in the left and right side of the webpage

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -13,7 +13,7 @@ export const AppLayout: FC<AppLayoutProps> = ({ children }) => {
       <GlowingGridBackground />
       <div className="flex flex-col min-h-screen relative z-10">
         <Header />
-        <main className="flex-grow w-full px-4 py-8">
+        <main className="flex-grow w-full max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
           {children}
         </main>
         <Footer />
